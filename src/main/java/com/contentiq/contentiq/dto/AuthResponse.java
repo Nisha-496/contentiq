9dacbdf22f8eafa6ecf77c098853ddf8c487b289
Nisha-496 <pronisha3658@gmail.com>
2026-05-16 12:16:01 +0530

    add AuthResponse for jwt token return

---
+++ src/main/java/com/contentiq/contentiq/dto/AuthResponse.java
@@ -0,0 +1,19 @@
+package com.contentiq.contentiq.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+
+    private String token;
+    private String tokenType;
+    private String username;
+    private String userId;
+    private Long expiresInSeconds;
+}
