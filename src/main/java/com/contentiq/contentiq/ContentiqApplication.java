284cb5c0dfb030dcc2812d6763c582572db3baa3
Nisha-496 <pronisha3658@gmail.com>
2026-05-16 12:17:07 +0530

    enable scheduling on main application

--- src/main/java/com/contentiq/contentiq/ContentiqApplication.java
+++ src/main/java/com/contentiq/contentiq/ContentiqApplication.java
@@ -2,8 +2,10 @@ package com.contentiq.contentiq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ContentiqApplication {
 
 	public static void main(String[] args) {
