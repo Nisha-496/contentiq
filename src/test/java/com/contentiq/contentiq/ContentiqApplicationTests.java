a70a904153c8f09313f3d34387ff48a12f42f930
Nisha-496 <pronisha3658@gmail.com>
2026-05-16 11:43:02 +0530

    initial project setup

---
+++ src/test/java/com/contentiq/contentiq/ContentiqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contentiq.contentiq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentiqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
